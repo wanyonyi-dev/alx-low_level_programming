2b634db4c415b8df95f48ad37589c2f7988c55ee
wanyonyi-dev <wanyonyip148@gmail.com>
2023-06-30 04:28:44 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337
+ * @str: The string to encode
+ *
+ * Return: Pointer to the encoded string
+ */
+char *leet(char *str)
+{
+    int i, j;
+    char letters[] = "aAeEoOtTlL";
+    char leet[] = "4433007711";
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        j = 0;
+        while (letters[j] != '\0')
+        {
+            if (str[i] == letters[j])
+            {
+                str[i] = leet[j];
+                break;
+}
+j++;
+}
+i++;
+}
+
+return str;
+}
+
