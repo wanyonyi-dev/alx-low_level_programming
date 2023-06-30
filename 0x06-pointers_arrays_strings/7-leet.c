07a162d7e252ff05213284cb85bd39e30d4f0e8b
wanyonyi-dev <wanyonyip148@gmail.com>
2023-06-30 04:39:36 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,26 +8,24 @@
  */
 char *leet(char *str)
 {
-    int i, j;
-    char letters[] = "aAeEoOtTlL";
-    char leet[] = "4433007711";
+int i, j;
+char letters[] = "aAeEoOtTlL";
+char leet[] = "4433007711";
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        j = 0;
-        while (letters[j] != '\0')
-        {
-            if (str[i] == letters[j])
-            {
-                str[i] = leet[j];
-                break;
+i = 0;
+while (str[i] != '\0')
+{
+j = 0;
+while (letters[j] != '\0')
+{
+if (str[i] == letters[j])
+{
+str[i] = leet[j];
+break;
 }
 j++;
 }
 i++;
 }
-
-return str;
+return (str);
 }
-
