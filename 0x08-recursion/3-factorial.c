35cdc7146fa6a81455a81d5e69178fbcdad60c7d
wanyonyi-dev <wanyonyip148@gmail.com>
2023-07-05 08:59:23 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * factorial - find factorial of numbers
+ *
+ * @n: parameter
+ *
+ * Return: End program
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (1);
+}
+else
+{
+n = n *factorial(n - 1);
+return (n);
+}
+return (0);
+}
