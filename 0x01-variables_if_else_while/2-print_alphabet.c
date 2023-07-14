ced2dcbeb977c7de26f8357376398446d17ba78e
wanyonyi-dev <wanyonyip148@gmail.com>
2023-07-14 05:01:46 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main func
+ *
+ * Return: End the prog
+ */
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; ++letter)
+{
+printf("%c", letter);
+
+}
+return (0);
+}
