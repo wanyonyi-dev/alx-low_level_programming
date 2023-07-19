87a9920abfd197d174dcec25a46e9c80d8db2028
wanyonyi-dev <wanyonyip148@gmail.com>
2023-07-19 05:52:59 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * print_name - Function to call a function
+ *
+ * @name: Parameter 1
+ * @f: parameter function
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+f(name);
+}
