9e16919e91c41b5001c033e451797d5552e42e0c
wanyonyi-dev <wanyonyip148@gmail.com>
2023-07-19 09:09:55 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -6,12 +6,14 @@
  * @argc: The number of command line arguments (including program name).
  * @argv: An array of strings containing the command line arguments.
  *
- * Return: 0 on success, 1 for incorrect number of arguments, and 2 for negative number of bytes.
+ * Return:0
  */
-int main(int argc, char *argv[]) {
-if (argc != 2) {
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+{
 printf("Error\n");
-return 1;
+return (1);
 }
 
 int num_bytes = atoi(argv[1]);
@@ -19,15 +21,15 @@ int num_bytes = atoi(argv[1]);
 if (num_bytes < 0)
 {
 printf("Error\n");
-return 2;
+return (2);
 }
-
 int i;
 printf("%02x", ((unsigned char *)main)[0]);
-for (i = 1; i < num_bytes; i++) {
+for (i = 1; i < num_bytes; i++)
+{
 printf(" %02x", ((unsigned char *)main)[i]);
 }
 printf("\n");
 
-return 0;
+return (0);
 }
