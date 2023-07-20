d5c1d99724da44e4f0e74b6373151ca7a67f79cd
wanyonyi-dev <wanyonyip148@gmail.com>
2023-07-20 08:05:06 -0700

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - func
+ * @separator: parameter
+ * @n: parameter
+ * @...
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+
+va_list(num);
+i = n;
+va_start(num, n);
+while (i > 0)
+{
+printf("%d", va_arg(num, int));
+if (i != 1)
+{
+if (separator != NULL)
+{
+printf("%s", separator);
+}
+}
+i--;
+}
+putchar(10);
+}
