47488b2697594b25733a7562f6e9039bd30d1a5c
wanyonyi-dev <wanyonyip148@gmail.com>
2023-07-20 07:56:50 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - func
+ * @n: param
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i, sum, arg;
+va_list(num);
+va_start(num, n);
+i = n;
+sum = 0;
+if (n == 0)
+return (0);
+arg = va_arg(num, int);
+while (i > 0)
+{
+sum += arg;
+arg = va_arg(num, int);
+i--;
+}
+return (sum);
+va_end(num);
+}
