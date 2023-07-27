1b2c3b4839f5f6a61dee86917cad4493d214206e
wanyonyi-dev <wanyonyip148@gmail.com>
2023-07-27 07:31:13 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints "Hello, Holberton" followed by a new line
+ *              using the printf function.
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+    printf("Hello, Holberton\n");
+    return (0);
+}
+
