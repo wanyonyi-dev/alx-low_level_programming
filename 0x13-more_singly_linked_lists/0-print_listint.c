50849c1ecfb6a77d1abbdc496665ffaaa5733406
wanyonyi-dev <wanyonyip148@gmail.com>
2023-07-31 05:20:43 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint - function for printing
+ *
+ * @h: parameter
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+int n;
+
+n = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+n++;
+}
+return (n);
+}
