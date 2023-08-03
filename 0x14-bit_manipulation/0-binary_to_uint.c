9f232c9cc4449a582299714ad07fc036c4ce8a27
wanyonyi-dev <wanyonyip148@gmail.com>
2023-08-03 07:02:00 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * binary_to_uint - function
+ * Description: converts binary digit to unsigned int
+ * @b: pointer of type unsigned int
+ * Return:converted number
+ * created: by wanyonyi
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int i = 0, val = 0;
+
+if (b == NULL)
+return (0);
+for (; *(b + i); i++)
+{
+val = val << 1;
+if (*(b + i) == '1')
+val = val | 1;
+else if (*(b + i) != '0')
+return (0);
+}
+return (val);
+}
