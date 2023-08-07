d94763bf61d7413524cabab96a1732df56831004
wanyonyi-dev <wanyonyip148@gmail.com>
2023-08-07 10:29:22 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_file - function
+ * Description: function that creates a file
+ * @filename: pointer to name of file to create
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure, or if filename is null
+ * created: by wanyonyi
+ */
+int create_file(const char *filename, char *text_content)
+{
+int o, w, num = 0;
+if (filename == NULL)
+return (-1);
+if (text_content != NULL)
+{
+for (num = 0; text_content[num];)
+num++;
+}
+o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+w = write(o, text_content, num);
+if (o == -1 || w == -1)
+return (-1);
+close(o);
+return (1);
+}
