863b9f3c04931158171e988a8cac9c3650d86613
wanyonyi-dev <wanyonyip148@gmail.com>
2023-12-18 10:59:14 -0800

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,32 +1,34 @@
 #include "main.h"
+
 /**
- * append_text_to_file - function
- * Description:Appends text at the end of a file.
- * standard output.
- * @filename: Name of the file to append.
- * @text_content: null terminated string to write to the file.
- * Return: 1 on success, -1 on failure.
- * ceated: by wanyonyi
+ * append_text_to_file - appends text at the end of a file
+ * @filename: file to append the text to
+ * @text_content: content to append into the file
+ *
+ * Return: 1 on success and -1 on failure
+ * created by: wanyonyi
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd;
-int len = 0;
-int countw;
-if (filename == NULL)
+int fd, a, b = 0;
+
+if (!filename)
 return (-1);
+
 fd = open(filename, O_WRONLY | O_APPEND);
-if (fd == -1)
+if (fd < 0)
 return (-1);
-while (*(text_content + len) != '\0')
-len++;
-countw = write(fd, text_content, len);
-if (countw == -1)
+
+if (text_content)
 {
-close(fd);
-write(STDOUT_FILENO, "fail", 5);
+while (text_content[b])
+b++;
+a = write(fd, text_content, b);
+if (a != b)
 return (-1);
 }
+
 close(fd);
+
 return (1);
 }
