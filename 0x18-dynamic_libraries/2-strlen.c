7fdf3304afca09a5e2f3481333ed0454138a35e9
wanyonyi-dev <wanyonyip148@gmail.com>
2024-01-29 09:17:52 -0800

    changes

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - swap the values of two integers
+ * @s: pointer to a char
+ * Return: number of chars
+ */
+int _strlen(char *s)
+{
+	int acc = 0;
+
+	while (s[acc] != '\0')
+	{
+		acc++;
+	}
+
+	return (acc);
+}
